09fe6611334bb864bf5327e87901e1cd0004240d
juliobarbagallo <julio.barbagallo@gmail.com>
2022-02-23 14:58:18 -0300

    otro

--- my-gatsby-site/gatsby-config.js
+++ my-gatsby-site/gatsby-config.js
@@ -3,24 +3,26 @@ module.exports = {
       title: `new`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx",  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
+  plugins: ["gatsby-plugin-image",
+          "gatsby-plugin-sitemap", {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+              "icon": "src/images/icon.png"
+              }},
+          "gatsby-plugin-mdx",  {
+              resolve: 'gatsby-source-filesystem',
+              options: {
+                "name": "images",
+                "path": "./src/images/"
+              },
+              __key: "images"
+            },
+            {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
       "path": "./src/pages/"
     },
     __key: "pages"
-  }]
+  }, "gatsby-plugin-sharp",]
 };
