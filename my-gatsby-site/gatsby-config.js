e72a8e457607b4d65d35ac585053b2bba13b75a9
juliobarbagallo <julio.barbagallo@gmail.com>
2022-02-24 10:14:30 -0300

    trying again

--- my-gatsby-site/gatsby-config.js
+++ my-gatsby-site/gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+      title: "My First Gatsby Site",
     },
     plugins: [
-
-    ]
-}
+      "gatsby-plugin-image",
+      "gatsby-plugin-sharp",
+    ],
+  };
