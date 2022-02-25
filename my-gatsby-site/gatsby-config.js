0b7bfbbdd22a6c9a4909163d1940b2d5449ba287
juliobarbagallo <julio.barbagallo@gmail.com>
2022-02-25 12:57:49 -0300

    adds query to page components

--- my-gatsby-site/gatsby-config.js
+++ my-gatsby-site/gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
     plugins: [
       "gatsby-plugin-image",
       "gatsby-plugin-sharp",
-      "gatsby-transformer-sharp",
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      },
     ],
   };
--- my-gatsby-site/src/pages/index.js
+++ my-gatsby-site/src/pages/index.js
@@ -8,7 +8,8 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="../images/bla.jfif"
+        // src="../images/bla.jfif"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
       />
     </Layout>
   )
