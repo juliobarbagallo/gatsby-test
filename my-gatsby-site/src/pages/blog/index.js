bddb1c2da4af5bfceb9275c55c0ca5d23f942742
juliobarbagallo <julio.barbagallo@gmail.com>
2022-02-25 17:39:54 -0300

    Adds blogpage

--- my-gatsby-site/src/pages/blog/index.js
+++ my-gatsby-site/src/pages/blog/index.js
@@ -1,19 +1,19 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../components/layout'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
-const BlogPage = ({data}) => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {
-        data.allMdx.nodes.map((node) => (
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
@@ -26,11 +26,11 @@ export const query = graphql`
     allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
         frontmatter {
+          date(formatString: "MMMM D, YYYY")
           title
-          date(formatString: "DD-MMMM-YYYY")
         }
         id
-        body
+        slug
       }
     }
   }
