bd1405719cb30e8cb2ce601817bcc546bb46e27b
juliobarbagallo <julio.barbagallo@gmail.com>
2022-02-25 17:04:13 -0300

    Added mdx

--- my-gatsby-site/gatsby-config.js
+++ my-gatsby-site/gatsby-config.js
@@ -12,5 +12,6 @@ module.exports = {
           path: `${__dirname}/blog`,
         }
       },
+      "gatsby-plugin-mdx",
     ],
   };
--- my-gatsby-site/src/pages/blog.js
+++ my-gatsby-site/src/pages/blog.js
@@ -1,28 +1,36 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({data}) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
 
 export const query = graphql`
-query {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "DD-MMMM-YYYY")
+        }
+        id
+        body
       }
     }
   }
