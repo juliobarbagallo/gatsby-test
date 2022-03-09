21e64315dc979e7d03579e2c96b6fac17d5d1a8c
Julio Barbagallo <julio.barbagallo@gmail.com>
2022-03-09 15:46:20 -0300

    finished part 7

--- my-gatsby-site/gatsby-config.js
+++ my-gatsby-site/gatsby-config.js
@@ -1,17 +1,18 @@
 module.exports = {
-    siteMetadata: {
-      title: "My First Gatsby Site",
-    },
-    plugins: [
-      "gatsby-plugin-image",
-      "gatsby-plugin-sharp",
-      {
-        resolve: "gatsby-source-filesystem",
-        options: {
-          name: `blog`,
-          path: `${__dirname}/blog`,
-        }
+  siteMetadata: {
+    title: "My First Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
       },
-      "gatsby-plugin-mdx",
-    ],
-  };
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
+};
